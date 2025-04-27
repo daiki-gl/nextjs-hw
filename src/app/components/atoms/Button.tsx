7b19e8e2b429af8a865c0f6aff4e9eb3732bd97d
Daiki Yamamoto <0ey329231k1095v@gmail.com>
2025-04-28 03:02:23 +0900

    small change

--- src/app/components/atoms/Button.tsx
+++ src/app/components/atoms/Button.tsx
@@ -8,7 +8,7 @@ export default function Button({ text, onClick, disabled = false, outline = fals
             disabled={disabled}
             className={`cursor-pointer  
                 ${outline ? 'outline-1 w-48 text-left flex-1/2' : ''}
-                ${noDeco ? 'text-blue-500 hover:text-gray-300 underline inline-block mr-20 text-left hover:!bg-transparent' : ''}
+                ${noDeco ? 'text-blue-500 hover:text-gray-300 underline inline-block mr-22 text-left hover:!bg-transparent' : ''}
                 ${outline == false && noDeco == false ? 'bg-blue-500 rounded-md px-4 py-2 mr-6 mt-4 text-white' : ''}
                 ${disabled ? 'opacity-50 !cursor-not-allowed bg-gray-500' : 'hover:outline-white hover:bg-white hover:text-black'} 
                 `}
